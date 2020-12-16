0bfc9b024771f7e1948cbbdaa9a933f8555151ec
2chainzbear <jameswilcock8@gmail.com>
2020-12-15 16:01:13 -0800

    added progress storage in case of force exit

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import requests
 import csv
 from bs4 import BeautifulSoup
 from tqdm import tqdm
+import re
+import os
 def getmp3(url):
     html = get(url)
     soup = Soup(html)
@@ -19,16 +21,14 @@ def parsexml(filename):
         content = f.read()
     soup = BeautifulSoup(content, "lxml")
     items = soup.findAll('item')
-    print(items)
     itemlist = []
     for item in items:
         itemdict = {}
         itemdict['url'] = item.find('guid').contents
         itemdict['title'] = item.find('title').contents
         date = item.find('pubdate').contents
-        #2006-16-Mar-Title.mp3
         date = date[0].split()
-        date = f"{date[3]}-{date[1]}-{date[2]}"
+        date = f"{date[3]}-{date[2]}-{date[1]}"
         itemdict['date'] = date
         itemlist.append(itemdict)
     return itemlist
@@ -46,17 +46,30 @@ def loadcsv(filename):
         infolist = []
         for row in csv_reader:
             if linecount != 0:
-                infolist.append([row[0].lstrip("['").rstrip("']"),row[1].lstrip("['").rstrip("']"),row[2]])
+                charlist = ['"','[',']','\'']
+                url = re.sub("|".join(charlist),"",row[0])
+                url = url[1:-1]
+                title = re.sub("|".join(charlist),"",row[1])
+                title = title[1:-1]
+                date = row[2]
+                infolist.append([url,title,date])
             linecount += 1
     return infolist
 def downloadpodcast(data):
-    for item in tqdm(data):
+    if os.pathexists('progress.txt'):
+        with open('progress.txt', 'r') as f:
+            start = int(f.readline())
+    else:
+        start = 0
+    counter = start
+    for item in tqdm(data[start:]):
         r = requests.get(item[0])
         #2006-16-Mar-Title.mp3
         open(f"downloads/{item[2]}-{item[1]}.mp3","wb").write(r.content)
-#podcasts = parsexml('econtalk.xml')
+        open(f"progress.txt","w").write(str(counter))
+        counter += 1
+podcasts = parsexml('econtalk.xml')
 #print(podcasts)
-#writetocsv(podcasts,'econ.csv')
+writetocsv(podcasts,'econ.csv')
 data = loadcsv('econ.csv')
 downloadpodcast(data)
-mp3 = getmp3('https://www.econtalk.org/rob-wiblin-and-russ-roberts-on-charity-science-and-utilitarianism/')
